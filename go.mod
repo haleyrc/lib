31f8b1e7bf3767c4819f7376ccc770ad6948fd85
Ryan Haley <me@ryanchaley.com>
2026-01-14 15:07:54 -0500

    feat(log): Add the `log` library for standardized logging
    This package was previously available at https://github.com/haleyrc/log.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/haleyrc/lib
 
 go 1.25.0
 
-require golang.org/x/crypto v0.47.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+)
---
+++ log/config.go
@@ -0,0 +1,35 @@
+package log
+
+import "io"
+
+type config struct {
+	w          io.Writer
+	debug      bool
+	freezeTime bool
+}
+
+// An Option is a function that modifies the default handler configuration.
+type Option func(*config)
+
+// Debug forces a handler to output debug lines.
+func Debug() Option {
+	return func(cfg *config) {
+		cfg.debug = true
+	}
+}
+
+// FreezeTime forces a handler to use a constant timestamp when emitting log
+// lines. This is only useful for testing where deterministic output is
+// required.
+func FreezeTime() Option {
+	return func(cfg *config) {
+		cfg.freezeTime = true
+	}
+}
+
+// Output sets the io.Writer to write messages to.
+func Output(w io.Writer) Option {
+	return func(cfg *config) {
+		cfg.w = w
+	}
+}
