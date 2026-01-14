33c991cbe9d25762d99d25905fc0064c7e361ede
Ryan Haley <me@ryanchaley.com>
2026-01-14 15:08:48 -0500

    feat(router): Add the `router` package for simple routing
    This package was previously available at
    https://github.com/haleyrc/router.

---
+++ router/router.go
@@ -0,0 +1,42 @@
+// Package router provides an http.Handler implementation with a nice API for
+// registering routes.
+package router
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Router is a wrapper around an http.ServeMux that provides a slightly nicer
+// API for registering routes.
+type Router struct {
+	mux *http.ServeMux
+}
+
+// New creates a new Router.
+func New() Router {
+	return Router{mux: http.NewServeMux()}
+}
+
+// GET registers a route at the provided path that responds to GET requests.
+func (router Router) GET(path string, h http.HandlerFunc) {
+	router.mux.HandleFunc(fmt.Sprintf("GET %s", path), h)
+}
+
+// POST registers a route at the provided path that responds to POST requests.
+func (router Router) POST(path string, h http.HandlerFunc) {
+	router.mux.HandleFunc(fmt.Sprintf("POST %s", path), h)
+}
+
+// ServeFiles serves static files located in `dir` and accessed via requests
+// whose path starts with `prefix`.
+func (router Router) ServeFiles(prefix, dir string) {
+	route := fmt.Sprintf("GET %s", prefix)
+	fs := http.FileServer(http.Dir(dir))
+	router.mux.Handle(route, http.StripPrefix(prefix, fs))
+}
+
+// ServeHTTP implements the http.Handler interface.
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router.mux.ServeHTTP(w, r)
+}
